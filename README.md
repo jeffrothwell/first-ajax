1d5ddfa83ec359c90ec8a58409de1dace8cb7a89
jeffrothwell <rothwelljeff@gmail.com>
2018-02-13 15:14:04 -0500

    complete up to step 3

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # Your First (Several) Asynchronous Requests
 
 [See assignment in Alexa.](https://alexa.bitmaker.co/wdi/67/assignments/2055/latest)
+
+Answers:
+1. GET method used
+1. 136ms to complete
+1. 200 status code - OK
+1. NOT sure about this in FireFox...  Chrome says "other" for initiator, and document as the type.  Still doesn't answer the question.
--- ajax.js
+++ ajax.js
@@ -1,5 +1,25 @@
 document.addEventListener("DOMContentLoaded", function() {
 
-  /* Your code goes here */
+  rootButtonEl = document.querySelector('button[name="root-button"]')
+  pingPongButtonEl = document.querySelector('button[name="ping-pong-button"]')
+
+  rootButtonEl.addEventListener('click', function(){
+    $.ajax({
+      url: "http://first-ajax-api.herokuapp.com/",
+      method: "get",
+      dataType: "text"
+    });
+  });
+
+  pingPongButtonEl.addEventListener('click', function(){
+    $.ajax({
+      url: "http://first-ajax-api.herokuapp.com/ping",
+      method: "get",
+      dataType: "text"
+    }).done(function(responseData){
+      console.log(responseData);
+    });
+  });
+
 
 });
