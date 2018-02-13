d4be69d0b9429eaef9b07de126fad37b6833f6f7
jeffrothwell <rothwelljeff@gmail.com>
2018-02-13 15:22:31 -0500

    complete set 4

--- ajax.js
+++ ajax.js
@@ -13,11 +13,15 @@ document.addEventListener("DOMContentLoaded", function() {
 
   pingPongButtonEl.addEventListener('click', function(){
     $.ajax({
-      url: "http://first-ajax-api.herokuapp.com/ping",
+      url: "http://first-ajax-api.herokuapp.com/pong",
       method: "get",
       dataType: "text"
     }).done(function(responseData){
       console.log(responseData);
+    }).fail(function(){
+      var failMsgEl = document.createElement('p');
+      failMsgEl.innerHTML = "Sorry, I have failed.  I will try harder next time."
+      document.querySelector('#step3456').appendChild(failMsgEl);
     });
   });
 
